23c92ea85e23f009be1d46e1fe0203b8fe427fc7
chasomin <somin49@naver.com>
2024-01-17 22:19:17 +0900

    Initial Commit

---
+++ MediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaApp
+//
+//  Created by 차소민 on 1/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
