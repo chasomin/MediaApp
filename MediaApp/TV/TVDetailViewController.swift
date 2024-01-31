e98ad942da0becae7db67c9f99a4ca20aa2dbf58
chasomin <somin49@naver.com>
2024-01-31 21:03:42 +0900

    데이터 요청 (그룹), 데이터 enum 생성

--- MediaApp/TV/TVDetailViewController.swift
+++ MediaApp/TV/TVDetailViewController.swift
@@ -9,32 +9,22 @@ import UIKit
 import Kingfisher
 import SnapKit
 
+enum TVData {
+    static var detail = Result(id: 0, backdrop: "", poster: "", firstAirDate: "", name: "", originalName: "", overview: "", voteAverage: 0, episodeRunTime: [], genres: [], lastEpisodeToAir: Episode(air_date: ""), nextEpisodeToAir: Episode(air_date: ""), networks: [])
+    static var cast = AggregateCredits(cast: [])
+    static var recommand =  Media(results: [])
+    
+    static let all: [Any] = [TVData.detail, TVData.cast, TVData.recommand]
+}
+
 class TVDetailViewController: BaseViewController {
     
     let tableView = UITableView()
-    var data = Media(results: [])
-    var cast = AggregateCredits(cast: [])
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let group = DispatchGroup()
-        group.enter()
-        MediaAPIManager.shard.fetchAggregateCreditsTV(id: 30983) { result in
-            self.cast = result
-            group.leave()
-        }
         
-        
-        group.enter()
-        MediaAPIManager.shard.fetchRecommandationTV(id: 30983) { result in
-            self.data = result
-            group.leave()
-        }
-        
-        group.notify(queue: .main) {
-            self.tableView.reloadData()
-        }
+        fetchData()
         
     }
     
@@ -52,7 +42,6 @@ class TVDetailViewController: BaseViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.backgroundColor = .clear
-//        tableView.rowHeight = 400
         
         tableView.separatorStyle = .none
 
@@ -62,24 +51,58 @@ class TVDetailViewController: BaseViewController {
     }
 }
 
+extension TVDetailViewController {
+    func fetchData() {
+        let group = DispatchGroup()
+        
+        group.enter()
+        MediaAPIManager.shard.fetchDetailsTV(id: 30983) { result in
+            
+            TVData.detail = result
+            group.leave()
+        }
+        
+        group.enter()
+        MediaAPIManager.shard.fetchAggregateCreditsTV(id: 30983) { result in
+            TVData.cast = result
+            group.leave()
+        }
+        
+        group.enter()
+        MediaAPIManager.shard.fetchRecommandationTV(id: 30983) { result in
+            TVData.recommand = result
+            group.leave()
+        }
+
+        group.notify(queue: .main) {
+            self.tableView.reloadData()
+        }
+
+    }
+}
+
 extension TVDetailViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        TVData.all.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             
             let cell = tableView.dequeueReusableCell(withIdentifier: TVDetailTableViewCell.id, for: indexPath) as! TVDetailTableViewCell
             
+            cell.configureCell(data: TVData.detail)
+            
             return cell
-
             
         } else if indexPath.row == 1 {
             let cell = tableView.dequeueReusableCell(withIdentifier: TVCastTableViewCell.id, for: indexPath) as! TVCastTableViewCell
             cell.collectionView.tag = indexPath.row
+            
             cell.collectionView.delegate = self
             cell.collectionView.dataSource = self
+            
             cell.collectionView.register(TVCastCollectionViewCell.self, forCellWithReuseIdentifier: TVCastCollectionViewCell.id)
+            
             cell.collectionView.reloadData()
 
             return cell
@@ -101,6 +124,16 @@ extension TVDetailViewController: UITableViewDelegate, UITableViewDataSource {
         }
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            250
+        } else if indexPath.row == 1{
+            250
+        } else {
+            400
+        }
+    }
+
     
 }
 
@@ -108,9 +141,9 @@ extension TVDetailViewController: UITableViewDelegate, UITableViewDataSource {
 extension TVDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView.tag == 1 {
-            cast.cast.count
+            TVData.cast.cast.count
         } else {
-            data.results.count
+            TVData.recommand.results.count
         }
     }
     
@@ -118,41 +151,22 @@ extension TVDetailViewController: UICollectionViewDelegate, UICollectionViewData
         if collectionView.tag == 1 {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TVCastCollectionViewCell.id, for: indexPath) as! TVCastCollectionViewCell
             
-            let url = Constants.Image.ImageBaseURL.baseURL + (cast.cast[indexPath.item].profile_path ?? "")
-            
-            cell.imageView.kf.setImage(with: URL(string: url))
-            cell.nameLabel.text = cast.cast[indexPath.item].name
-            cell.roleLabel.text = cast.cast[indexPath.item].roles.first?.character
+            cell.configureCell(data: TVData.cast, index: indexPath.row)
             
             return cell
             
         } else {
             
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaCollectionViewCell.id, for: indexPath) as! MediaCollectionViewCell
-            
-            let item = data.results
-            
+            let item = TVData.recommand.results
             let url = URL(string: Constants.Image.ImageBaseURL.baseURL + (item[indexPath.item].backdrop ?? item[indexPath.item].poster ?? ""))
             
-            cell.posterImageView.kf.setImage(with: url, placeholder: Constants.Image.PlaceholderImage.image)
-            cell.nameLabel.text = item[indexPath.item].name
-            cell.overviewLabel.text = item[indexPath.item].overview
-            cell.dateLabel.text = item[indexPath.item].firstAirDate
-            cell.voteAverageLabel.text = String(item[indexPath.item].vote)
-            
+            cell.configureCell(item: item, url: url, index: indexPath.item)
+
             return cell
         }
         
         
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
-            100
-        } else if indexPath.row == 1{
-            200
-        } else {
-            400
-        }
-    }
 }
