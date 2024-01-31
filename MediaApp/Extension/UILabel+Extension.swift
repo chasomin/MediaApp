18f0f827da35bfcc9961570f111602f3a16ce31f
chasomin <somin49@naver.com>
2024-01-31 21:01:58 +0900

    Tv Detail 레이블 extension 함수 구현

---
+++ MediaApp/Extension/UILabel+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UILabel+Extension.swift
+//  MediaApp
+//
+//  Created by 차소민 on 1/31/24.
+//
+
+import UIKit
+
+extension UILabel {
+    func setTVDetailLabel() {
+        font = .systemFont(ofSize: 13)
+        textAlignment = .left
+        textColor = .white
+    }
+}
