d4cfc13a3e77552efdb5cd5ce34d886c94b6fb60
chasomin <somin49@naver.com>
2024-02-05 21:51:22 +0900

    userDefaultsManager 생성

---
+++ MediaApp/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,31 @@
+//
+//  UserDefaultsManager.swift
+//  MediaApp
+//
+//  Created by 차소민 on 2/5/24.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    
+    private init() { }
+    
+    enum UDKey: String {
+        case search
+    }
+    
+    let ud = UserDefaults.standard
+    
+    var search: [String] {
+        get {
+            ud.stringArray(forKey: UDKey.search.rawValue) ?? []
+        }
+        set {
+            ud.setValue(newValue, forKey: UDKey.search.rawValue)
+        }
+    }
+    
+    
+}
